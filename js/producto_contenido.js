112738838711ef2e2a5d8a5654b2212bbfe86c5f
Melissa <mnjuarez@scl.edu.gt>
2023-03-23 10:50:59 -0600

    Se arreglaron configuraciones

--- js/producto_contenido.js
+++ js/producto_contenido.js
@@ -4,8 +4,8 @@ export function merch (numero){
     let PRODUCTO = `
     <div class="H">
         <img src="./img/foto.png" alt="">
-        <h4 class="merchh ${numero}>${our_products[numero].titulo}</h4>
-        <h5 class="merchh ${numero}>${our_products[numero].contenido}</h5>
+        <h4 class="merchh ${numero}">${our_products[numero].titulo}</h4>
+        <h5 class="merchh ${numero}">${our_products[numero].contenido}</h5>
     </div>
     
     `;
